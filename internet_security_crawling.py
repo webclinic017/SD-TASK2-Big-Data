df096cfcadab071ed534b5a5fe1fb27982ddcec2
Usama <ubenabdelkrim2@gmail.com>
2021-06-17 02:58:02 +0200

    parse csv

--- internet_security_crawling.py
+++ internet_security_crawling.py
@@ -16,8 +16,7 @@ from hashlib import md5
 import uuid
 from flask import Flask, render_template, request
 import operator
-from dask.utils import SerializableLock
-
+import ast
 
 BUCKET_NAME='sd-task2'
 
@@ -26,11 +25,11 @@ twitter_consumer_secret = "UVPSLbfTudhGa4j1MlsmDA6KxXJUeY7mqGQkprdsHJD1rFcJH6"
 twitter_access_token = "1313145032-gdwPOWniKGX9jbOwlUs1fqqJuDfLzue17FdNDUD"
 twitter_access_token_secret = "s5YDPEylUR9hWuuNIXNIRmgTXoVkBKFwavxke2u8O49pi"
 
-header = ['id', 'user_name', 'name', 'account_created_at', 'location', 'URL', 'political_analysis', 'religion_analysis', 'protected', 'geo_enabled ', 'geo', 'coordinates', 'description', 'post_id', 'post_created_at', 'post_text']
-
+posts_header = ["political_analysis", "religion_analysis", "protected", "geo_enabled", "geo", "coordinates", "description", "post_id", "post_created_at", "post_text"]
+profile_header = ["id", "user_name", "name", "account_created_at", "location", "URL"]
 auth = tweepy.OAuthHandler(twitter_consumer_key, twitter_consumer_secret)
 auth.set_access_token(twitter_access_token, twitter_access_token_secret)
-api = tweepy.API(auth) 
+api = tweepy.API(auth)
 
 fexec = lithops.FunctionExecutor(backend='ibm_cf', runtime='usipiton/lithops-custom1-runtime-3.9:0.1')
 app = Flask(__name__)
@@ -38,67 +37,91 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     return render_template('index.html')
+
+def facebook_profile_crawler(facebook_token):
+    graph = facebook.GraphAPI(facebook_token)
+    field = ['id,name,link,location']
+    profile = graph.get_object(id="me",fields=field)
+    row = [str(profile['id']).replace(',', " "), "null", str(profile['name']).replace(',', " "), "null", str(profile['location']).replace(',', " "), str(profile['link']).replace(',', " "), 'null', 'null',
+        "null"]
+    return row
+
+def twitter_profile_crawler(twitter_screen_name):
+    user = api.get_user(twitter_screen_name)
+    row=[str(user.id_str), str(user.screen_name).replace(',', " "), str(user.name).replace(',', " "), str(user.created_at).replace(',', " "), str(user.location).replace(',', " "), 
+       str(user.url).replace(',', " "), str(user.protected).replace(',', " "), str(user.geo_enabled).replace(',', " "), 
+       str(user.description).replace(',', " ")]
+    return row
+
 def twitter_crawler_function(twitter_screen_name, storage):
     posts=[]
     for post in api.user_timeline(screen_name=twitter_screen_name, count=200, include_rts = False, tweet_mode = 'extended'):
         posts.append(post)
     return posts
 
-def facebook_crawler_processing_function(facebook_token, storage):
+def facebook_posts_crawler(facebook_token, storage):
     graph = facebook.GraphAPI(facebook_token)
-    field = ['id,name,email,birthday,link,location,gender,hometown,age_range,education,political,religion,posts']
+    field = ['posts']
     profile = graph.get_object(id="me",fields=field)
-    posts_dict={}
-    row = []
+
     for post in profile['posts']['data']:
         if post.get('message'):
-            row=["null", str(profile['name']).replace(',', " "), " ", str(profile['location']).replace(',', " "), str(profile['link']).replace(',', " "), 'null', 'null', 'null', 'null', 
-            'null', 'null', 'null', str(profile['id']).replace(',', " "), "null", str(post.get('message').replace(',', " "))]
-
+            row = [" ", " ", str(post.get('id')), str(profile.get('created_time')), "null", "null", str(post.get('message').replace(',', " "))]
     return row
 
-def twitter_preprocessing_function(post, storage):
+def twitter_posts_preprocessing(post, storage):
     row = []
     try:
-        row=[str(post.user.screen_name).replace(',', " "), str(post.user.name).replace(',', " "), str(post.user.created_at).replace(',', " "), str(post.user.location).replace(',', " "), 
-        str('https://twitter.com/'+post.user.screen_name).replace(',', " "), ' ', ' ', str(post.user.protected).replace(',', " "), str(post.user.geo_enabled).replace(',', " "), 
-        str(post.geo).replace(',', " "), str(post.coordinates).replace(',', " "), str(post.user.description).replace(',', " "), str(post.id).replace(',', " "), str(post.created_at).replace(',', " "), 
-        str(post.full_text.replace(',', " "))]
+        row=[" ", " ", str(post.id).replace(',', " "), str(post.created_at).replace(',', " "), str(post.geo).replace(',', " "), str(post.coordinates).replace(',', " "), str(post.full_text.replace(',', " "))]
     except:
-        row=[str(post.user.screen_name).replace(',', " "), str(post.user.name).replace(',', " "), str(post.user.created_at).replace(',', " "), str(post.user.location).replace(',', " "), 
-        str('https://twitter.com/'+post.user.screen_name).replace(',', " "), ' ', ' ', str(post.user.protected).replace(',', " "), str(post.user.geo_enabled).replace(',', " "), 
-        str(post.geo).replace(',', " "), str(post.coordinates).replace(',', " "), str(post.user.description).replace(',', " "), str(post.id).replace(',', " "), str(post.created_at).replace(',', " "), 
-        str(post.text.replace(',', " "))]
-    
+        row=[" ", " ", str(post.id).replace(',', " "), str(post.created_at).replace(',', " "), str(post.geo).replace(',', " "), str(post.coordinates).replace(',', " "), str(post.text.replace(',', " "))]
     return row
+
+def merge_and_push_info(posts, tprofile, fprofile, path, storage):
+    posts_dict = {}
+    profile_dict = {}
+
+    profile_dict = {}
+    posts_dict = {}
+
+    for k in profile_header:
+        profile_dict[k] = []
+    for k in posts_header:
+        posts_dict[k] = []
+    
+    for profile_field, column_name in zip(tprofile, profile_header):
+        profile_dict[column_name].append(profile_field)
+    for profile_field, column_name in zip(fprofile, profile_header):
+        profile_dict[column_name].append(profile_field)
+    for field, column_name in zip(posts, posts_header):
+        posts_dict[column_name].append(field)
+    posts_dict = do_predictions(posts_dict)
+    id = storage.put_cloudobject(write_csv_dict(profile_dict | posts_dict), BUCKET_NAME, path+".csv")
+    return id
 
 def do_predictions(posts_dict):
     political_pred = political_analysis(posts_dict.values())
     religion_pred = religion_analysis(posts_dict.values())
-    output = io.StringIO()
-    w = csv.DictWriter(output, posts_dict.keys())
     for post in posts_dict.values():
         if(len(post) > 14):
             post[5] = political_pred
             post[6] = religion_pred
-    w.writerow(posts_dict)
-    return output.getvalue()
+    return posts_dict
 
-def total_scoring(id, storage):
+def write_csv_dict(complete_dict):
+    output = io.StringIO()
+    w = csv.DictWriter(output, complete_dict.keys())
+    for v in complete_dict.values():
+        w.writerow(v)
+    return str.encode(output.getvalue())
+
+def total_scoring(obj_id, storage):
     score = 0
-    posts = storage.get_cloudobject(id)
+    posts = storage.get_cloudobject(obj_id).decode()
+    print(ast.literal_eval(posts))
     i=0
     valid_post=False
-    posts = posts.split('\n')
-    posts.extend(posts.split('\n'))
-    post = " "
-    while(i<len(posts) and valid_post is False):
-        if(posts[i] != ' ' and posts[i] != '' and i != 0):
-            post = posts[i]
-            if(len(post) > 14):
-                valid_post=True
-        i+=1
-    score+=profile_scoring(post.split(','))
+    posts = posts.split("\",")
     score+=predictions_scoring(post.split(','))
     return score
 
@@ -113,13 +136,13 @@ def total_scoring(id, storage):
 #     location-->+5 points
 #     political idelogy != neutral-->+40 points
 #     religion ideology not neutral-->+40 points
-def profile_scoring(post):
+def profile_scoring(twitter_post, facebook_post):
     score=0
     #print(len(post[3]))
-    if(len(post) > 14):
-        if(len(post[3]) > 0 ): score+=5           #location
-        if(post[7] !=  "True"): score+=10      #public profile
-        if((post[8] != "False") and (post[9] != "None") and (post[10] != "None")): score+=90     #geo enabled and the position is visible
+    if(len(twitter_post) > 14):
+        if(len(twitter_post[3]) > 0 or len(facebook_post[3]) > 0): score+=5           #location
+        if(twitter_post[7] !=  "True" or facebook_post[7] !=  "True"): score+=10      #public profile
+        if((twitter_post[8] != "False") and (twitter_post[9] != "None") and (twitter_post[10] != "None")): score+=90     #geo enabled and the position is visible, twitter excl
     return score
 
 def predictions_scoring(post):
@@ -199,18 +222,25 @@ def religion_analysis(posts):
         return max_value
     else:
         return "neutral"
-
-def merge_posts(posts, path, storage):
-    posts_dict = {}
-    for post, column_name in zip(posts, header):
-        posts_dict[column_name] = post
-    id = storage.put_cloudobject(do_predictions(posts_dict), BUCKET_NAME, path+".csv")
-    return id
     
+def get_valid_post(posts):
+    post = " "
+    valid_post = False
+    i = 0
+    while(i<len(posts) and valid_post is False):
+        if(posts[i] != ' ' and posts[i] != '' and i != 0):
+            post = posts[i]
+            if(len(post) > 14):
+                valid_post=True
+        i+=1
+    return post
 
 @app.route('/do_security_analysis')
 def do_security_analysis():
     registred_users = {}
+    posts = []
+    posts2 = []
+    score = 0
     #avatar = request.args.get('avatar')
     avatar="usipiton"
     storage = Storage()
@@ -218,18 +248,29 @@ def do_security_analysis():
         path = registred_users.get(avatar)
     else:
         path = registred_users[avatar] = str(uuid.uuid4())
+    storage = Storage()
+    twitter_username = request.args.get('tname')
+    facebook_token = request.args.get('fname')
+    if twitter_username is not None:
+        twitter_profile=twitter_profile_crawler(twitter_username)
     
-    fexec.call_async(twitter_crawler_function, request.args.get('tname'))
-    posts = fexec.get_result()
-    fexec.map(twitter_preprocessing_function, posts)
-    posts = fexec.get_result()
-    fexec.call_async(facebook_crawler_processing_function, (request.args.get('fname')))
-    posts2 = fexec.get_result()
-    posts = posts + posts2
-    fexec.call_async(merge_posts, (posts, path))
-    obj_id = fexec.get_result()
-    fexec.map(total_scoring, obj_id)
-    return str(2)
+        posts=twitter_crawler_function(twitter_username, storage)
+        twitter_posts=[]
+        for post in posts:
+            twitter_posts+=twitter_posts_preprocessing(post, storage)
+            
+
+    if facebook_token is not None:
+        facebook_profile=facebook_profile_crawler(facebook_token)
+        facebook_posts=facebook_posts_crawler(facebook_token, storage)
+
+    posts = twitter_posts + facebook_posts
+    obj_id=merge_and_push_info(posts, twitter_profile, facebook_profile, path, storage)
+
+    profile_scoring(twitter_profile, facebook_profile)
+
+    total_scoring(obj_id, storage)
+    return str(1)
 
 if __name__ == '__main__':
   app.run(debug=True)  
